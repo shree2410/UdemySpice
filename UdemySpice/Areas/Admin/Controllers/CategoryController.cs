052c033507337f770e5938a074dfbbe128489e56
Devishree Venkatesh <shree2410@gmail.com>
2021-11-28 22:10:32 -0500

    Add project files.

---
+++ UdemySpice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UdemySpice.Data;
+
+namespace UdemySpice.Areas.Admin.Controllers
+{
+    [Area("Admin")] //telling this controller is for admin.
+    public class CategoryController : Controller
+    {
+        /*to retrieve everything inside dbcontext from category and display,
+        D.I play integral part to fetch data.In startup.cs in services
+        that's why we added ApplicationDbContext in Connection String*/
+
+        private readonly ApplicationDbContext _db;//local obj to retrieve(AppDbcon db)
+
+        //create Ctor(fetch from the ioc container)
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;//this will retrieve value
+        }
+        //Get everything from db and pass it to view
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
---
+++ UdemySpice/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemySpice.Models;
+
+namespace UdemySpice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
